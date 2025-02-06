3eeb02cef75bb49b54db9a9b216f4dcfdee527a5
satyamjha <satyamjhaactor07@gmail.com>
2025-02-06 14:11:19 +0530

    done project

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,15 @@
 
+import React, { useState } from 'react';
+
+const Selection = ({ applyColor }) => {
+    
+  const [style, setStyle] = useState({ background: "" });
+
+  return (
+    <div className="fix-box" style={style} onClick={() => applyColor(setStyle)}>
+      Click to Change Color
+    </div>
+  );
+};
+
+export default Selection;
